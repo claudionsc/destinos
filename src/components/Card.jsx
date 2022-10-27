c36555f607f6bb45181f6f546a1d9503df1e4fae
alvigabriel <claudio.alves20112011@hotmail.com>
2022-10-27 13:52:57 -0300

    refatorando

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Card({children}){
+    return (
+        <h1 style={{position: 'absolute'}}>{children}</h1>
+    )
+}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,10 +3,25 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { FormStyle, FormS } from "./Styles";
 
-function Form(){
+import { Card } from "./Card";
+import { useForm } from "react-hook-form";
+
+function Form(props){
+
+    const {register, handleSubmit} = useForm()
 
     const [paises, setPaises] = useState([])
     const [cidades, setCidades] = useState([])
+
+    // const [nome, setNome] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [fone, setFone] = useState("");
+    // const [cpf, setCpf] = useState("");
+    const [error, setError] = useState("");
+
+    const [info, setInfo] = useState(false)
+
+    
 
 
     useEffect(() => {
@@ -20,7 +35,7 @@ function Form(){
         axios.get("https://amazon-api.sellead.com/city")
         .then(response => {
             setCidades(response.data)
-            console.log(cidades)
+            // console.log(cidades)
         })
 
     }, [])
@@ -38,61 +53,103 @@ function Form(){
         return lista
     }
     VerificaCodigo(codigo, cidades)
-    console.log(lista)
+    // console.log(lista)
+
+    let card = {}
+  const onSubmit = (e) => {
+    if ( !e.nome | !e.email | !e.fone | !e.cpf) {
+        setError("Preencha todos os campos");
+        
+        
+    }else{
+        setInfo(true)
+        setError("")
+        card = {
+            nome: e.nome,
+            cidade: e.cidades,
+            // pais: e.target.value
+        }
+
+    }
+    
+    console.log(e)
+  }  
+
 
   
 
     return(
         <FormStyle>
-            <FormS>
+            <FormS onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-g">
                     <div className="dados">
                         <p>Dados Pessoais</p>
                         <input className="input" type="text"
-                        placeholder="Nome" />
+                        placeholder="Nome"
+                        {...register("nome")}
+                        // value={nome}
+                        />
                         <input className="input" type="email"
-                        placeholder="Email" />
+                        placeholder="Email"
+                        {...register("email")}
+                        // value={email}
+                        />
                         <input className="input" type="text" 
-                        placeholder="Telefone" />
+                        placeholder="Telefone"
+                        {...register("fone")}
+                        // value={fone}
+                        />
                         <input className="input" type="text"
-                        placeholder="CPF" />
+                        placeholder="CPF"
+                        {...register("cpf")}
+                        // value={cpf}
+                        />
                     </div>
+
                     <div className="destinos">
+
                         <p>Destinos de interesse</p>
-                        <select name="paises" onChange={(e) => setCodigo(e.target.value)}>
+                        <select onChange={(e) => setCodigo(e.target.value)} >
+                        <option title={"name"} key={"select"} value={"select"}>Selecione o país</option>
                             {paises.map(país => {
                                 return (
-                                <option 
+                                <option
                                 key={país.id} 
                                 value={país.code}
+                                name={"pais"}
+                                // {...register(this.país.name_ptbr)}
+                                
                                 >
-                                    {país.name_ptbr}
+                                {país.name_ptbr}
                                 </option>
                                 )})
                             }
                         </select>
-                        <select name="cidade">
 
-                           
-                            
+
+                        <select name="cidade">
                         {lista.map(list => {
                             return (
                                 <option 
                                 key={list.id} 
-                                value={list.country_code}>
+                                value={list.name}
+                                {...register("listacidade")}
+                                >
                                     {list.name !== null ? 
                                      <>{list.name}</> 
                                     :<>{list.name_ptbr}</> }
                                 </option>
                                 )})
-                            }
-                          
-                        
+                        }
                         </select>
-
                     </div>
+
+                {info === true &&
+                    <Card >{card.nome}</Card>
+                }
                 </div>
-                    <input className="btn" type="submit" placeholder="nada" />
+                    <label>{error}</label>
+                    <input className="btn" type="submit" placeholder="Enviar" />
             </FormS>
         </FormStyle>
 
