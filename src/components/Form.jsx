e7de897e466a53349ac3c36b96f6d0a32d71cbbe
alvigabriel <claudio.alves20112011@hotmail.com>
2022-12-21 09:10:03 -0300

    minify

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,29 +4,30 @@ import { useEffect, useState } from "react";
 import { FormStyle, FormS } from "./Styles";
 import { Card } from "./Card";
 import { useForm } from "react-hook-form";
+import InputMask from "react-input-mask";
 
 function Form(){
 
+    // SELECIONANDO HOOK PARA CONTROLE DOS FORMULÁRIOS
+
     const {formState: { errors }, register, handleSubmit} = useForm()
 
+    // HOOKS PARA CONTROLE DE ESTADO DA REQUISIÇÃO À API
     const [paises, setPaises] = useState([])
-    const [cidades, setCidades] = useState([])
+    const [cidades, setCidades] = useState([]);
 
-    // const [nome, setNome] = useState("");
-    // const [email, setEmail] = useState("");
-    // const [fone, setFone] = useState("");
-    // const [cpf, setCpf] = useState("");
+    // HOOK PARA VALIDAR ERRO
     const [error, setError] = useState("");
 
-    const [info, setInfo] = useState(false)
+    // HOOK PARA RENDERIZAÇÃO CONDICIONAL DO CARD
+    const [card, setCard] = useState(false)
 
+    // HOOOKS PARA EXIBIR DADOS NO CARD
     const [getPais, setGetPaís] = useState(null)
     const [getCidades, setGetCidades] = useState(null)
     const [getNome, setGetNome] = useState(null)
 
-    
-
-
+    // MÉTODOS GET PARA API
     useEffect(() => {
         axios.get("https://amazon-api.sellead.com/country")
         .then(response => {
@@ -43,26 +44,20 @@ function Form(){
     }, [])
 
 
-
+    // FUNÇÃO PARA ESCOLHA DO PAÍS/CIDADE, ALTERAR ESTADO DO CARD E ADICIONAR NOME PARA SER EXIBIDO NO CARD
   const onSubmit = (e) => {
-    // if ( !e.nome | !e.email | !e.fone | !e.cpf | getPais === null | getCidades === null) {
-    //     setError("Preencha todos os campos");
+    if ( getPais === null | getCidades === null) {
+        setError("Preencha todos os campos");
         
         
-    // }else{
-        setInfo(true)
+    }else{
+        setCard(true)
         setGetNome(() => e.nome)
         setError("")
-    // }  
-    
-    console.log(getPais)
-    console.log(getCidades)
-    console.log(getNome)
+    }  
   }
 
-
-
-  
+//   FORMULÁRIOS
 
     return(
         <FormStyle>
@@ -75,11 +70,9 @@ function Form(){
                         {...register("nome", {
                             required: "Inserir nome",
                           })}
-                        // value={nome}
                         />
                         {errors.nome && <p className="errors">{errors.nome.message}</p>}
                         <input className="input" type="email"
-                        // name="email"
                         placeholder="Email"
                         {...register("email", {
                             required: "Inserir email",
@@ -88,26 +81,32 @@ function Form(){
                               message: "endereço de email inválido"
                             }
                           })}
-                        // value={email}
                         />
                         {errors.email && <p className="errors">{errors.email.message}</p>}
-                        <input className="input" type="text" 
+                        <InputMask className="input" type="tel" 
+                        pattern="(\([0-9]{2}\))\s([9]{1})?([0-9]{5})-([0-9]{4})"
                         placeholder="Telefone"
+                        mask="(99) 99999-9999"
                         {...register("fone", {
                             required: "Inserir telefone",
                             pattern: {
-                              value: /^(?:\+)[0-9]{2}\s?(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/i,
                               message: "Telefone inválido"
                             }
                           })}
-                        // value={fone}
                         />
                         {errors.fone && <p className="errors">{errors.fone.message}</p>}
-                        <input className="input" type="text"
+                        <InputMask className="input" type="text"
                         placeholder="CPF"
-                        {...register("cpf")}
-                        // value={cpf}
-                        />
+                        pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" 
+                        mask="999.999.999-99"
+                        {...register("cpf", {
+                            required: "Inserir o cpf",
+                            pattern: {
+                              message: "Cpf inválido"
+                            }
+                          })}
+                          />
+                          {errors.cpf && <p className="errors">{errors.cpf.message}</p>}
                     </div>
 
                     <div className="destinos">
@@ -120,7 +119,9 @@ function Form(){
                                 <option
                                 key={país.code} 
                                 value={país.name_ptbr}
-                                
+                                {...register("país", {
+                                    required: "Selecione o país",
+                                  })}
                                 >
                                 {país.name_ptbr}
                                 </option>
@@ -136,6 +137,9 @@ function Form(){
                                 <option 
                                 key={cidade.id} 
                                 value={cidade.name}
+                                {...register("cidade", {
+                                    required: "Selecione a cidade",
+                                  })}
                                 >
                                    {cidade.name_ptbr}
                                 </option>
@@ -144,14 +148,14 @@ function Form(){
                         </select>
                     </div>
 
-                {info === true &&
+                {card === true &&
                     <Card >
-                        <h3>Parabéns, {getNome}! O destino escolhido foi {getPais} para país ou {getCidades} para cidade.</h3>
-                        <button className="btn" onClick={() => setInfo(false)}>x</button>
+                        <h3>Boa escolha, {getNome}! Destinos escolhidos: {getPais} para país ou {getCidades} para cidade.</h3>
+                        <button className="btn2" onClick={() => setCard(false)}>x</button>
                     </Card>
                 }
                 </div>
-                    <label>{error}</label>
+                    <label className="errors label-err"><h3>{error}</h3></label>
                     <input className="btn" type="submit" placeholder="Enviar" />
             </FormS>
         </FormStyle>
